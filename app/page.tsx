7b757c3e341c982cfd2308cb216e9c94a0ec749a
LleonardoDuarte <leopd_00@hotmail.com>
2023-09-15 13:19:49 -0300

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,150 @@
-import Image from 'next/image'
+import {
+  Home as Homeicon,
+  Search,
+  Library,
+  ChevronLeft,
+  ChevronRight,
+  Play,
+} from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className="flex items-center gap-3">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+          </div>
+          <nav className="space-y-5 mt-10">
+            <a
+              href=""
+              className="flex items-center gap-2 text-xs font-semibold text-zinc-100"
+            >
+              <Homeicon />
+              Home
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 text-xs font-semibold text-zinc-100"
+            >
+              <Search />
+              Search
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 text-xs font-semibold text-zinc-200"
+            >
+              <Library />
+              Your library
+            </a>
+          </nav>
+          <nav className="mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3 ">
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Hot Hits brasil
+            </a>
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Creme
+            </a>
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Hip Hop 2000/2010
+            </a>
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Sensação
+            </a>
+            <a href="" className="text-small text-zinc-400">
+              Aquelas
+            </a>
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Melhores do Samba
+            </a>
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Revelação ao vivo no Morro
+            </a>
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Antigas
+            </a>
+            <a href="" className="text-small text-zinc-400 hover:text-zinc-100">
+              Rap
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          <div className="flex flex-items-center gap-3">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-6">
+            <a
+              href="#"
+              className="bg-white/10 flex items-center gap-4 group rounded overflow-hidden hover:bg-white/20 transition-colors"
+            >
+              <img className="w-21 h-24" src="/album.jpg" alt="capa" />
+              <strong>Nada como um dia</strong>
+              <button className="p-4 rounded-full bg-green-400 text-black ml-auto mr-10 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/10 rounded flex items-center gap-4 hover:bg-white/20 group transition-colors"
+            >
+              <img className="w-21 h-24" src="/album2.jpg" alt="capa" />
+              <strong>Get rich or die trying</strong>
+              <button className="p-4 rounded-full bg-green-400 text-black ml-auto mr-10 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/10 rounded flex items-center gap-4 hover:bg-white/20 group transition-colors"
+            >
+              <img className="w-21 h-24" src="/album3.jpg" alt="capa" />
+              <strong>Me against the world</strong>
+              <button className="p-4 rounded-full bg-green-400 text-black ml-auto mr-10 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/10 rounded flex items-center gap-4 hover:bg-white/20 group transition-colors"
+            >
+              <img className="w-21 h-24" src="/album4.jpg" alt="capa" />
+              <strong>Sobrevivendo no inferno</strong>
+              <button className="p-4 rounded-full bg-green-400 text-black ml-auto mr-10 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/10 rounded flex items-center gap-4 hover:bg-white/20 group transition-colors"
+            >
+              <img className="w-21 h-24" src="/album5.jpg" alt="capa" />
+              <strong>Rap é compromisso</strong>
+              <button className="p-4 rounded-full bg-green-400 text-black ml-auto mr-10 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/10 rounded flex items-center gap-4 hover:bg-white/20 group transition-colors"
+            >
+              <img className="w-21 h-24" src="/album6.jpg" alt="capa" />
+              <strong>509-e</strong>
+              <button className="p-4 rounded-full bg-green-400 text-black ml-auto mr-10 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
